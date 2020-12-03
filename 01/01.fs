6cb6cb9b3a3d40d56f10d8beb39dd2be7001eee9
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-03 15:28:01 -0800

    :baguette_bread:
    I got it right but then I accidentally forgot to submit the
    position instead of the index so I was under the answer by one.
    That... was really painful.

--- 01/01.fs
+++ 01/01.fs
@@ -3,16 +3,40 @@ open System
 [<Literal>]
 let InputFile = "input.txt"
 
-let parenToInt (p: byte) =
+let parenToInt (p: char) =
    match Convert.ToChar p with
    | '(' -> +1
    | ')' -> -1
    | _   ->  0
 
-let main() =
+let flip f a b = f b a
+
+let append a = flip (+) a
+
+let chars = (
    InputFile
    |> IO.File.ReadAllBytes
+   |> Array.map Convert.ToChar
+)
+
+let aaaa b a = ((b, a), a)
+
+let main() =
+   // part 1
+   chars
    |> Array.sumBy parenToInt
+   |> Convert.ToString
+   |> (+) "Part 1: Santa is on floor "
+   |> append " by the end of the route. "
+   |> Console.WriteLine
+
+   chars
+   |> Array.indexed
+   |> Array.mapFold (fun acc (idx, char) -> acc + (parenToInt char) |> aaaa idx) 0
+   |> fst
+   |> Array.find (fun (_, lvl) -> lvl < 0)
+   |> fun (idx, lvl) -> (Convert.ToString idx, Convert.ToString lvl)
+   |> fun (idx, lvl) -> "Part 2: Santa enters the basement (" + lvl + ") on INDEX " + idx
    |> Console.WriteLine
 
 main()
