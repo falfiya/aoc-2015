cfd23b6aae4208187f8409f94835dcbc7d4b07e7
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-04 13:02:03 -0800

    2015.12.04 done!
    Hey, that's not bad, not bad.

--- 04/04.fs
+++ 04/04.fs
@@ -8,29 +8,42 @@ let md5Hash (input : string) =
    |> md5.ComputeHash
 
 let startsWithFiveZeros (bytes: byte[]) =
-   (bytes.[0] ||| bytes.[1] ||| (bytes.[2] &&& 0xF0uy)) = 0uy
+   bytes.[0] ||| bytes.[1] ||| (bytes.[2] &&& 0xF0uy) = 0uy
+
+let startsWithSixZeros (bytes: byte[]) =
+   bytes.[0] ||| bytes.[1] ||| (bytes.[2]) = 0uy
 
 let secretKey = "iwrupvqb"
 
 let makeKey (i: uint) = secretKey + string i
 
-let rec mine (i: uint) =
-   if
-      makeKey i
-      |> md5Hash
-      |> startsWithFiveZeros
-   then
-      i
-   else
-      mine (i + 1u)
+let mineWith fn =
+   let rec mine = fun (i: uint) ->
+      let key = makeKey i
+      if
+         key
+         |> md5Hash
+         |> fn
+      then
+         key
+      else
+         mine (i + 1u)
+   mine
 
 let flip f a b = f b a
 let (++) a = flip (+) a
 
-let baToString (ary: byte[]) = BitConverter.ToString(ary)
+let baToString (ary: byte[]) = BitConverter.ToString(ary).Replace("-", "")
 
-let keyEnd = mine 0u
-let key = makeKey keyEnd
-let hash = md5Hash key
+let mine5 = mineWith startsWithFiveZeros
+let mine6 = mineWith startsWithSixZeros
 
-Console.WriteLine (sprintf "%s = %s" key (baToString hash))
+let key5 = mine5 0u
+let hash5 = key5 |> md5Hash |> baToString
+
+Console.WriteLine (sprintf "%-18s = %s" key5 hash5)
+
+let key6 = mine6 0u
+let hash6 = key6 |> md5Hash |> baToString
+
+Console.WriteLine (sprintf "%-18s = %s" key6 hash6)
