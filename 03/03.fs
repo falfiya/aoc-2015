a8273a1bfc2b0162c85b85582ec2c81e9678fd6d
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-03 21:50:31 -0800

    Completely misunderstanding part 2
    Well, I completely misunderstood what I was supposed to do but I
    still wrote some cool code and learned about tuples of length
    three. :scringe:

--- 03/03.fs
+++ 03/03.fs
@@ -13,32 +13,68 @@ let increment (loc: Location) (map: SantaMap) =
    | Some count -> map.Remove(loc) |> ignore; map.Add(loc, count + 1u)
    | None -> map.Add(loc, 1u)
 
-let walk (loc: Location, map: SantaMap) (arrow: char) =
-   let (x, y) = loc;
-   let newloc =
+let santaWalk (santa: Location, map: SantaMap) (arrow: char) =
+   let (x, y) = santa;
+   let newSanta =
       match arrow with
       | '^' -> (x, y + 1)
       | '>' -> (x + 1, y)
       | 'v' -> (x, y - 1)
       | '<' -> (x - 1, y)
-      | _ -> loc
-   (newloc, increment newloc map)
+      | _ -> santa
+   (newSanta, increment newSanta map)
 
-let (finalLoc, finalMap) =
+let startingMap0: SantaMap = Map.empty.Add((0, 0), 1u)
+
+let (finalLoc0, finalMap0) =
    file
-   |> Array.fold walk ((0, 0), Map.empty.Add((0, 0), 1u))
+   |> Array.fold santaWalk ((0, 0), startingMap0)
 
+(*
 finalLoc
 |> string
 |> (+) "Santa ended his trip at "
 |> Console.WriteLine
+*)
 
-finalMap
+finalMap0
 |> Map.toSeq
 |> Seq.map snd
-|> Seq.filter (fun count -> count > 1u)
+|> Seq.filter (fun count -> count > 0u)
 |> Seq.length
 |> string
-|> (+) "There were "
-|> (++) " houses that had more than one present"
+|> (+) "With just Santa, there were "
+|> (++) " houses that had at least one present."
+|> Console.WriteLine
+
+let robotWalk (robot: Location, map: SantaMap) (arrow: char) =
+   let (x, y) = robot;
+   let newRobot =
+      match arrow with
+      | '^' -> (x, y - 1)
+      | '>' -> (x - 1, y)
+      | 'v' -> (x, y + 1)
+      | '<' -> (x + 1, y)
+      | _ -> robot
+   (newRobot, increment newRobot map)
+
+let bothWalk ((santa: Location, robot: Location), map0: SantaMap) arrow =
+   let (newSanta, map1) = santaWalk (santa, map0) arrow
+   let (newRobot, map2) = robotWalk (robot, map1) arrow
+   ((newSanta, newRobot), map2)
+
+let startingMap1 = Map.empty.Add((0, 0), 2u) // since there are 2 santas now
+
+let state1 = (((0, 0), (0, 0)), startingMap1)
+
+let (finalLoc1, finalMap1) = file |> Array.fold bothWalk state1
+
+finalMap1
+|> Map.toSeq
+|> Seq.map snd
+|> Seq.filter (fun count -> count > 0u)
+|> Seq.length
+|> string
+|> (+) "With robot Santa, there are "
+|> (++) " houses that had at least one present."
 |> Console.WriteLine
