d9293c78f16f20c208a3b8bc2341b94dbe04e1de
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-27 20:05:26 -0800

    parsing works but now what?
    Classic case of not being able to see the forest for the trees.

--- 07/07.shared.fs
+++ 07/07.shared.fs
@@ -4,19 +4,22 @@ open System
 module Instructions =
    type Symbol = uint16
 
+   // for my sake, an expression is either a reference or a value
    type Exp =
-      | Byte   of byte
-      | Ref    of Symbol
-      | Direct of Exp
-      | And    of Exp * Exp
-      | Or     of Exp * Exp
-      | Not    of Exp
-      | LShift of Exp * amount: Exp
-      | RShift of Exp * amount: Exp
+   | Val    of uint
+   | Ref    of Symbol
 
-   let parseByte (s: string) =
+   type RValue =
+   | Init   of Exp
+   | And    of Exp * Exp
+   | Or     of Exp * Exp
+   | Not    of Exp
+   | LShift of Exp * amount: Exp
+   | RShift of Exp * amount: Exp
+
+   let parseUint (s: string) =
       try
-         Some <| byte s
+         Some <| uint s
       with _ ->
          None
 
@@ -27,47 +30,41 @@ module Instructions =
       | 2 -> Some (chars.[0] <<< 8 ||| chars.[1])
       | _ -> None
 
-   exception ExpTokenException of string
-
    let Exp (s: string) =
-      match parseByte s with
-      | Some byte -> Byte byte
+      match parseUint s with
+      | Some thing -> Val thing
       | None ->
          match parseSymbol s with
          | Some symbol -> Ref symbol
-         | None -> failwith <| ("No matches found for " + s)
+         | None -> failwith ("No matches found for " + s)
 
    type Instruction = {
       LValue: Symbol;
-      RValue: Exp;
+      RValue: RValue;
    }
-
-   exception InvalidInstructionException of string
 
    let fromString (s: string) =
       let revWords = s.Split " " |> Array.rev
 
       if revWords.Length < 3 then
-         raise <| InvalidInstructionException
-            "There must be at least 3 words per instruction"
+         failwith "There must be at least 3 words per instruction"
 
       let symbol =
          match parseSymbol revWords.[0] with
          | Some symbol -> symbol
-         | None -> raise <| InvalidInstructionException ("Invalid symbol: " + revWords.[0])
+         | None -> failwith ("Invalid symbol: " + revWords.[0])
 
-      let exp =
+      let rhs =
          match revWords.[2..] with
-         | [| a |]              -> Direct (Exp a)        // a
+         | [| a |]              -> Init   (Exp a)        // a
          | [| a; "NOT" |]       -> Not    (Exp a)        // NOT a
          | [| b; "AND"; a |]    -> And    (Exp a, Exp b) // a AND b
          | [| b; "OR" ; a |]    -> Or     (Exp a, Exp b) // a OR  b
          | [| b; "LSHIFT"; a |] -> LShift (Exp a, Exp b) // a LSHIFT b
          | [| b; "RSHIFT"; a |] -> RShift (Exp a, Exp b) // a RSHIFT b
-         | _ -> raise <| InvalidInstructionException
-                  "Unknown instruction type"
+         | _ -> failwith "Unknown instruction type"
 
-      { LValue = symbol; RValue = exp }
+      { LValue = symbol; RValue = rhs }
 
 let filename = __SOURCE_DIRECTORY__ + "/wires.txt"
 
