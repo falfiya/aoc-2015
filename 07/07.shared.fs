f42fe44a072adba2f8815e68626c67416d0d324f
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-27 19:55:08 -0800

    nearly works
    getting the parsing in order.
    looks like values can actually be above 255.

--- 07/07.shared.fs
+++ 07/07.shared.fs
@@ -2,24 +2,45 @@ module Shared
 open System
 
 module Instructions =
-   type LValue = string
+   type Symbol = uint16
 
-   type RValue =
-      | Direct of byte
-      | And    of LValue * LValue
-      | Or     of LValue * LValue
-      | Not    of LValue
-      | LShift of LValue * amount: byte
-      | RShift of LValue * amount: byte
+   type Exp =
+      | Byte   of byte
+      | Ref    of Symbol
+      | Direct of Exp
+      | And    of Exp * Exp
+      | Or     of Exp * Exp
+      | Not    of Exp
+      | LShift of Exp * amount: Exp
+      | RShift of Exp * amount: Exp
+
+   let parseByte (s: string) =
+      try
+         Some <| byte s
+      with _ ->
+         None
+
+   let parseSymbol (s: string): Symbol Option =
+      let chars = s |> Seq.map uint16 |> Seq.toArray
+      match chars.Length with
+      | 1 -> Some (chars.[0] <<< 8)
+      | 2 -> Some (chars.[0] <<< 8 ||| chars.[1])
+      | _ -> None
+
+   exception ExpTokenException of string
+
+   let Exp (s: string) =
+      match parseByte s with
+      | Some byte -> Byte byte
+      | None ->
+         match parseSymbol s with
+         | Some symbol -> Ref symbol
+         | None -> failwith <| ("No matches found for " + s)
 
    type Instruction = {
-      LValue: LValue;
-      RValue: RValue;
+      LValue: Symbol;
+      RValue: Exp;
    }
-
-   let bite s =
-      Console.WriteLine ("bite " + s)
-      byte s
 
    exception InvalidInstructionException of string
 
@@ -30,20 +51,23 @@ module Instructions =
          raise <| InvalidInstructionException
             "There must be at least 3 words per instruction"
 
-      let lvalue = revWords.[0]
+      let symbol =
+         match parseSymbol revWords.[0] with
+         | Some symbol -> symbol
+         | None -> raise <| InvalidInstructionException ("Invalid symbol: " + revWords.[0])
 
-      let rvalue =
+      let exp =
          match revWords.[2..] with
-         | [| state |] -> Direct <| bite state
-         | [| a; "NOT" |] -> Not a
-         | [| b; "AND"; a |] -> And (a, b)
-         | [| b; "OR" ; a |] -> Or  (a, b)
-         | [| amount; "LSHIFT"; lvalue |] -> LShift (lvalue, byte amount)
-         | [| amount; "RSHIFT"; lvalue |] -> RShift (lvalue, byte amount)
+         | [| a |]              -> Direct (Exp a)        // a
+         | [| a; "NOT" |]       -> Not    (Exp a)        // NOT a
+         | [| b; "AND"; a |]    -> And    (Exp a, Exp b) // a AND b
+         | [| b; "OR" ; a |]    -> Or     (Exp a, Exp b) // a OR  b
+         | [| b; "LSHIFT"; a |] -> LShift (Exp a, Exp b) // a LSHIFT b
+         | [| b; "RSHIFT"; a |] -> RShift (Exp a, Exp b) // a RSHIFT b
          | _ -> raise <| InvalidInstructionException
                   "Unknown instruction type"
 
-      { LValue = lvalue; RValue = rvalue }
+      { LValue = symbol; RValue = exp }
 
 let filename = __SOURCE_DIRECTORY__ + "/wires.txt"
 
